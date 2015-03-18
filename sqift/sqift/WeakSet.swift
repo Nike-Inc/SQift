8de2630440db00377c7056d5c77e84e780e8c96e
Dave Camp <dave@thinbits.com>
2015-03-17 18:43:03 -0700

    Added WeakSet

---
+++ sqift/sqift/WeakSet.swift
@@ -0,0 +1,78 @@
+//
+//  WeakSet.swift
+//  sqift
+//
+//  Created by Dave Camp on 3/17/15.
+//  Copyright (c) 2015 thinbits. All rights reserved.
+//
+
+import Foundation
+
+private class Weak<T: AnyObject>
+{
+    weak var value: T?
+    
+    init(value: T)
+    {
+        self.value = value
+    }
+}
+
+public class WeakSet<T: AnyObject>
+{
+    private var weakObjects = Array<Weak<T>>()
+    
+    public init()
+    {
+        
+    }
+    
+    public func addObject(object: T!)
+    {
+        compact()
+        weakObjects.append(Weak(value: object))
+    }
+    
+    public func removeObject(object: T!)
+    {
+        weakObjects = weakObjects.filter( { $0.value != nil && $0.value! !== object } )
+    }
+    
+    private func compact()
+    {
+        weakObjects = weakObjects.filter( { $0.value != nil } )
+    }
+    
+    public func dump()
+    {
+        println("---")
+        if weakObjects.isEmpty
+        {
+            println("WeakSet is empty")
+        }
+        else
+        {
+            for weakObject in weakObjects
+            {
+                println("\(weakObject.value)")
+            }
+        }
+    }
+    
+    public func containsObject(object: T!) -> Bool
+    {
+        compact()
+
+        var result = false
+        for weakObject in weakObjects
+        {
+            if weakObject.value === object
+            {
+                result = true
+                break
+            }
+        }
+        
+        return result
+    }
+}
