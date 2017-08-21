3e4ae98d6b2bb0b8683f858f3680618c51832ef6
Christian Noon <christian.noon@nike.com>
2017-08-21 16:13:58 -0700

    Fixed bug where busy timeouts weren’t working as documented on iOS 10 and 11.

--- Tests/Tests/Connection/Functions/BusyTests.swift
+++ Tests/Tests/Connection/Functions/BusyTests.swift
@@ -34,27 +34,48 @@ class BusyTestCase: BaseConnectionTestCase {
 
             let readConnection = try Connection(storageLocation: storageLocation, readOnly: true)
 
-            let expectation = self.expectation(description: "agents should be retrieved from database")
+            let readExpectation = self.expectation(description: "agents should be retrieved from database")
+            let checkpointExpectation = self.expectation(description: "checkpoint operation should succeed")
+
             var agents: [Agent]?
             var checkpointError: Error?
 
             // When
-            try connection.busyHandler(.timeout(2.0)) // throws SQLITE_BUSY error if this is disabled
+            let timeout: TimeInterval
+
+            // CN (8/21/17) - I can't find a way around this issue. It seems that iOS 10 and 11 might have a
+            // simulator bug or potentially a bug in SQLite where the timeout is actually in nanoseconds instead
+            // of milliseconds. By bumping the value up, the tests do pass. Because of this, I'd strongly advocate
+            // that people only ever use custom busy handlers that track their own timing rather than using the
+            // timeout busy handler. Again, this may only be an issue in the unit tests and the simulator.
+            //
+            // I also tested using `PRAGMA busy_timeout = 2000` and the same behavior is observed. I can't find
+            // any information about this online which is odd, but we should really avoid using this in production.
+            if #available(iOS 10.0, macOS 10.12, tvOS 10.0, *) {
+                timeout = 20_000.0
+                // try connection.execute("PRAGMA busy_timeout = 2000")
+            } else {
+                timeout = 2.0
+            }
+
+            try connection.busyHandler(.timeout(timeout)) // throws SQLITE_BUSY error if this is disabled
 
             DispatchQueue.utility.async {
                 do {
                     agents = try readConnection.query("SELECT * FROM agents")
-                    expectation.fulfill()
+                    readExpectation.fulfill()
                 } catch {
                     // No-op
                 }
             }
 
-            DispatchQueue.utility.asyncAfter(seconds: 0.001) {
+            DispatchQueue.utility.asyncAfter(seconds: 0.01) {
                 do {
                     _ = try self.connection.checkpoint(mode: .truncate)
+                    checkpointExpectation.fulfill()
                 } catch {
                     checkpointError = error
+                    checkpointExpectation.fulfill()
                 }
             }
 
