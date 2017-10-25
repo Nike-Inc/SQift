03d82d1110803eea0f478c223f88b9073b32cc5b
Christian Noon <christian.noon@nike.com>
2017-10-25 12:29:50 -0700

    Updated the LICENSE to New BSD and also updated all copyright headers to match.

--- Tests/Extensions/ProcessInfoExtension.swift
+++ Tests/Extensions/ProcessInfoExtension.swift
@@ -1,9 +1,11 @@
 //
 //  ProcessInfoExtension.swift
-//  SQift
 //
-//  Created by Christian Noon on 9/21/17.
-//  Copyright © 2017 Nike. All rights reserved.
+//  Copyright 2015-present, Nike, Inc.
+//  All rights reserved.
+//
+//  This source code is licensed under the BSD-stylelicense found in the LICENSE
+//  file in the root directory of this source tree.
 //
 
 import Foundation
